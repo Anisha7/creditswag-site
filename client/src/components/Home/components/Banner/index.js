1eab9d79d994406dd23f298b3e3169f88c30bad0
Anisha Jain <anishajain@outlook.com>
2019-10-29 11:49:58 -0700

    style curve added

--- client/src/components/Home/components/Banner/index.js
+++ client/src/components/Home/components/Banner/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import phones from '../../../../assets/Phones.png';
+import curve from '../../../../assets/bottomCurve.png';
 import './styles.css';
 
 class Banner extends Component {
     render() {
-        return (<div className="banner">
-                    <div className="slogan">
-                        <h1>Save $$.</h1>
-                        <h2>Dumb simple.</h2>
-                        <button>JOIN THE WAITLIST</button>
+        return (<div className="bannerBg">
+                    <div className="banner">
+                        <div className="slogan">
+                            <h1>Save $$.</h1>
+                            <h2>Dumb simple.</h2>
+                            <button>JOIN THE WAITLIST</button>
+                        </div>
+                        <img src={ phones } alt="Credit swag screenshots" />
                     </div>
-                    <img src={ phones } alt="Credit swag screenshots" />
+                    <img src={ curve } alt="style curve" />
                 </div>)
     }
 }
