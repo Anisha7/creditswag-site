81124778d580831c6fa3e26d800d61d1cda6b3fc
Anisha Jain <anishajain@outlook.com>
2019-10-29 15:24:35 -0700

    menu button

--- client/src/components/HomeWrapper/components/Navbar/index.js
+++ client/src/components/HomeWrapper/components/Navbar/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './styles.css';
 import logo from '../../../../assets/CreditSwagLogo.png';
 import logoText from '../../../../assets/LogoText.png';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Navbar extends Component {
     render() {
@@ -15,11 +17,13 @@ class Navbar extends Component {
                 </li>   
             </ul>
             
-            <ul>
+            <ul className="options">
                 {/* TODO: replace to use redirect when routes are implemented */}
                 <li><a href="#item2">About us</a></li>
                 <li><a href="#item3">Our story</a></li>
                 <li className="waitlistButton"><a href="#item4">Join the waitlist</a></li>
+                {/* TODO: hamburger functionality */}
+                <li><a href="#"><FontAwesomeIcon className="barIcon" icon={ faBars } size="1x" /></a></li>
             </ul>
         </div>)
     }
