91a323d24762c62a4ea7893f1ec86ac6ffa13910
Anisha Jain <anishajain@outlook.com>
2019-10-29 12:00:57 -0700

    styles

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -8,6 +8,7 @@ class Home extends Component {
         return (
             <HomeWrapper>
                 <Banner />
+                <div> hello world</div>
             </HomeWrapper>)
     }
 }
