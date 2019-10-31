ee0c742759d2d5d98f20e02241f676f05fbb45aa
Anisha Jain <anishajain@outlook.com>
2019-10-31 15:27:38 -0700

    style updates

--- client/src/components/Home/components/InfoBlurb/index.js
+++ client/src/components/Home/components/InfoBlurb/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './styles.css';
-import illustration from '../../../../assets/Illustration.svg';
+import illustration from '../../../../assets/Illustration.png';
 
 class InfoBlurb extends Component {
     render() {
-        return (<div className="infoblurb">
-                    <h1>Reach your goals.</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Tempor enim libero amet, eu volutpat. Iaculis sit lacus id volutpat. 
-                        Suspendisse in facilisis hendrerit cras nunc. Aliquam magna malesuada 
-                        quisque vulputate ac, lorem consequat gravida ipsum.</p>
-                    <img src={ illustration } alt="illustration" />
+        return (<div className="infoblurb-wrapper">
+                    <div className="infoblurb">
+                        <h1>Reach your goals.</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                            Tempor enim libero amet, eu volutpat. Iaculis sit lacus id volutpat. 
+                            Suspendisse in facilisis hendrerit cras nunc. Aliquam magna malesuada 
+                            quisque vulputate ac, lorem consequat gravida ipsum.</p>
+                        <img src={ illustration } alt="illustration" />
+                    </div>
                 </div>)
     }
 }
