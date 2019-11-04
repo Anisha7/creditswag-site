6bd5f5e3be993c5c283c4eb542d01622b6567df7
Anisha Jain <anishajain@outlook.com>
2019-11-03 23:20:14 -0800

    fetch setup

--- client/src/components/WaitlistForm/index.js
+++ client/src/components/WaitlistForm/index.js
@@ -3,15 +3,45 @@ import './styles.css';
 
 // join waitlist form
 class WaitlistForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            error: false,
+        }
+    }
+
+    sendEmail() {
+        console.log("SENDING DATA...")
+        fetch("/", {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(this.state.email)
+        }).then((response) => response.json())
+          .then((data) => {
+              console.log("STATUS: ", data.status)
+              if (!data.status) {
+                this.setState({ error : true });
+              }
+            })
+          .catch((err) => {
+              console.log(err)
+              this.setState({ error : true });
+          })
+        
+    }
 
     render() {
+        // TODO: confirmation that email went through!
+        // TODO: error message if it errored
+
         if ( this.props.scroll ) {
             this.refs.waitlist.scrollIntoView({ behavior: "smooth" });
         }
         return (<div ref="waitlist" className="contact">
                     <div className="form">
-                        <input type="email" placeholder="email" />
-                        <button>JOIN THE WAITLIST</button>
+                        <input onChange={ (e) => this.setState( { email: e.target.value })} type="email" placeholder="email" />
+                        <button >JOIN THE WAITLIST</button>
                     </div>
                 </div>)
     }
