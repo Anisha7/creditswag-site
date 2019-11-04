415e51898957b23c19038da82bf1c8454d9e27fb
KJ Wilson <yourboykjdev@gmail.com>
2019-11-03 23:30:42 -0800

    added backend to site

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,78 @@
 var express = require('express');
 var router = express.Router();
+var email_validator = require("email-validator");
 
-// Example Route
-router.get('/', (req, res) => {
-  res.json("Hello World")
+// APIs
+const MAILCHIMP_INT = process.env.MAILCHIMP_INT
+const MAILCHIMP_KEY = process.env.MAILCHIMP_KEY
+const MANDRILL_KEY = process.env.MANDRILL_KEY
+const LIST_ID = process.env.LIST_ID
+const FROM_EMAIL = process.env.FROM_EMAIL
+
+// Email Services
+const mandrill = require('mandrill-api/mandrill');
+const mandrill_client = new mandrill.Mandrill(MANDRILL_KEY);
+
+// Email Route
+router.post('/', (req, res) => {
+  const { email } = req.body;
+
+  // Validate Email
+  if (!(email_validator.validate(email))) {
+    console.log("FAILED :(")
+    res.json({ status: false })
+    return
+  }
+
+  // Mailchimp Post Body
+  const data = {
+    email_address: email,
+    status: 'subscribed',
+  };
+
+  var options = {
+    method: 'POST',
+    url: 'https://' + MAILCHIMP_INT + '.api.mailchimp.com/3.0/lists/' + LIST_ID + '/members',
+    headers: {
+      Authorization: 'apikey ' + MAILCHIMP_KEY
+    },
+    body: postData
+  }
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+
+  res.json({ status: true })
+
+  // Send Email
+  var template_name = "credit-waitlist"
+  // var template_content = [{
+  //     "FNAME": firstName,
+  //   }];
+  var message = {
+      "from_email": FROM_EMAIL,
+      "from_name": "Credit Swag Inc.",
+      "to":[{"email": email}],
+      "subject": "Thank you for your interest in Credit Swag",
+  };
+
+  // Set time to send email
+  var d = new Date();
+  d = new Date(d.getTime() - 3000000);
+  var send_at = d.getFullYear().toString()+"-"+((d.getMonth()+1).toString().length==2?(d.getMonth()+1).toString():"0"+(d.getMonth()+1).toString())+"-"+(d.getDate().toString().length==2?d.getDate().toString():"0"+d.getDate().toString())+" "+(d.getHours().toString().length==2?d.getHours().toString():"0"+d.getHours().toString())+":"+((parseInt(d.getMinutes()/5)*5).toString().length==2?(parseInt(d.getMinutes()/5)*5).toString():"0"+(parseInt(d.getMinutes()/5)*5).toString())+":00";
+  var async = false;
+  var ip_pool = "Main Pool";
+
+  // SEND THE EMAIL
+  mandrill_client.messages.sendTemplate({"template_name": template_name, "message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
+    console.log(result);
+  }, function(e) {
+    // Mandrill returns the error as an object with name and message keys
+    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+  });
 });
 
 module.exports = router;
