b9fa472c3ea8aa6daa0aa447cdfb55cba9a2e814
Anisha Jain <anishajain@outlook.com>
2019-11-13 13:51:16 -0800

    info and about us

--- client/src/components/Home/components/InfoBlurb/index.js
+++ client/src/components/Home/components/InfoBlurb/index.js
@@ -6,8 +6,13 @@ class InfoBlurb extends Component {
     render() {
         return (<div className="infoblurb-wrapper">
                     <div className="infoblurb">
-                        <h1>Reach your goals.</h1>
-                        <p>Our mission is to make financing accessible and approachable for college students. Students are struggling to manage their money and have no incentive to learn about financial literacy. Our solution is an app that incentivizes them to make smarter financial decisions on a daily basis by teaching them money management as they spend.</p>
+                        {/* <h1>Reach your goals.</h1> */}
+                        <h1>We inspire <i>change</i> in financial decisions everyday.</h1>
+                        <p>What’s in your smart device could never be more important. Our phones now carry our lives
+                            from our identity to our social experience. Credit Swag’s team of experts thought it high priority
+                            to bring together a tool that will make your financial life simple, forward thinking and of course
+                            fun to use and learn.
+                            </p>
                         <img src={ illustration } alt="illustration" />
                     </div>
                 </div>)
--- client/src/components/OurStory/components/AboutUs/index.js
+++ client/src/components/OurStory/components/AboutUs/index.js
@@ -7,15 +7,20 @@ class AboutUs extends Component {
         return (<div className="aboutUs">
                     <div>
                         <h1>About us</h1>
-                        <h2>Built by students, for students</h2>
+                        <h2>Not your average thought leaders!</h2>
                         {/* Conquer Credit Management has served thousands of 
                             clients enabling and educating them on the importance 
                             of credit.  */}
-                        <p>After years of witnessing students struggle 
+                        {/* <p>After years of witnessing students struggle 
                             with money management, Angela Setters envisioned an app to 
                             help students learn about financial literacy in an easy, 
                             engaging process. She enlisted the help of student developers 
                             at Make School to accomplish her mission.</p>
+                        <p> */}
+                        <p>With over 50 years of combined wisdom and experience in education, finance, credit and
+                        technology the Credit Swag team is ready to help you understand how to use your money and
+                        credit as a wealth building tool for success!
+                        </p>
                     </div>
                     <img src={ illustration } alt="illustration" />
                 </div>)
